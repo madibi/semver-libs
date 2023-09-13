7ebae761b5616ef86d5d1917293006ade93d3aee
محمد ادیبی <mohammad.adibi@Farineh.local>
2023-09-13 12:41:12 +0330

    fix(button): add somee exclimet

---
+++ projects/input/src/lib/input.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-input',
+  template: `
+    <p>
+      input works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class InputComponent {
+
+}
